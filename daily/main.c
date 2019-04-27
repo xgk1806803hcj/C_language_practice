c737c2f4d30721054f8afe65164085e80b60485b
xgk1806803hcj <48375769+xgk1806803hcj@users.noreply.github.com>
2019-04-28 00:24:50 +0800

    Add files via upload

--- daily/main.c
+++ daily/main.c
@@ -1,86 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define len sizeof(struct student)
-struct student
-{
-   int num;
-   struct student *next;
-};
-struct student *creat(int a,struct student *head)
-{
-    int n=0;
-    struct student *p1,*p2;
-    p1=p2=(struct student*) malloc (len);
-    scanf("%d",&p1->num);
-    head=NULL;
-    while(p1->num!=0&&n<=a)
-    {
-        n+=1;
-        if(n==1)
-         head=p1;
-        else
-         p2->next=p1;
-        p2=p1;
-        p1=(struct student*) malloc (len);
-        scanf("%d",&p1->num);
-    }
-    p2->next=NULL;
-   return(head);
+#pragma warning(disable:4996)
+
+typedef struct _node {
+	int value;
+	struct _node * next;
+}Node , * NodePoint , * List;
+
+List Creat(int length) {
+	NodePoint ListHead = NULL;
+	NodePoint * Local=&ListHead;
+	while (length--) {
+		*Local = (NodePoint)malloc(sizeof(Node));
+		scanf("%d" , &(*Local)->value);
+		Local = &(*Local)->next;
+	}
+	*Local = NULL;
+	return ListHead;
 }
-void print(struct student *head)
-{
-    struct student *p;
-    p=head;
-    if(head!=NULL)
-    {
-        do
-        {
-            printf("%d ",p->num);
-            p=p->next;
-        }while(p!=NULL);
-        printf("\n");
-    }
+
+void Reverse(List * list) {
+	NodePoint Iter_list = (*list)->next;
+	NodePoint New_Head = *list;
+	while (Iter_list != NULL) {
+		NodePoint temp = Iter_list->next;
+		(*list)->next = Iter_list->next;
+		Iter_list->next = New_Head;
+		New_Head = Iter_list;
+		Iter_list = temp;
+	}
+	*list = New_Head;
 }
-struct student *fun(struct student *la,struct student *lb)
-{
-    struct student *p1,*p2,*p3;
-    struct student *head;
-    p1=la;
-    p2=lb;
-    if(p1->num<p2->num)
-      {head=p1;p3=p1;p1=p1->next;}
-    else
-      {head=p2;p3=p2;p2=p2->next;}
-    while(p1!=NULL&&p2!=NULL)
-    if(p1->num<p2->num)
-      {
-         p3->next=p1;
-         p1=p1->next;
-         p3=p3->next;
-      }
-      else
-      {
-         p3->next=p2;
-         p2=p2->next;
-         p3=p3->next;
-      }
-     if(p1==NULL&&p2!=NULL)
-     {
-         p3->next=p2;
-     }
-     else if(p1!=NULL&&p2==NULL)
-     {
-         p3->next=p1;
-     }
-     return(head);
+
+
+
+
+void PrintList(List list) {
+	while (list != NULL) {
+		printf("%d " , list->value);
+		list = list->next;
+	}
+	printf("\n");
 }
-int main()
-{
-   struct student *la,*lb;
-   la=creat(4,la);
-   print(la);
-   lb=creat(6,lb);
-   print(lb);
-   print(fun(la,lb));
-   printf("\n");
+
+
+void Merge(List * list1 , List * list2) {
+	//先逆置链表1
+	Reverse(list1);
+
+	/*
+	依次遍历链表2,插入到相应位置
+	(简单思维,因为如果已知链表2是递增的,逆置链表2后可以不重置Iter_list1的值,减少循环次数)
+	*/
+	NodePoint Iter_list2 = *list2;
+	while (Iter_list2 != NULL) {
+		NodePoint Iter_list1 = *list1;
+		NodePoint Last = NULL;
+		while (Iter_list1 != NULL) {
+			if (Iter_list2->value < Iter_list1->value) {
+				Last = Iter_list1;
+				Iter_list1 = Iter_list1->next;
+			}
+			else {
+				NodePoint temp = Iter_list2->next;
+				if (Last == NULL) {
+					Iter_list2->next = Iter_list1;
+					(*list1) = Iter_list2;
+				}
+				else {
+					Iter_list2->next = Iter_list1;
+					Last->next = Iter_list2;
+				}
+				Iter_list2 = temp;
+				break;
+			}
+		}
+	}
+	*list2 = NULL;
+
+}
+int main() {
+	freopen("in.txt" , "r" , stdin);
+	//freopen("out.txt" , "w" , stdout);
+	List t1 , t2;
+	t1 = Creat(4);
+	PrintList(t1);
+	t2 = Creat(4);
+	PrintList(t2);
+	Merge(&t1 , &t2);
+	PrintList(t1);
+	return 0;
 }
